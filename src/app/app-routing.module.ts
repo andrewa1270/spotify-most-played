8214c20c01c3fa2a2fb73db4d44eb58a58a796bb
Andrew Aguh <andrew.aguh@gmail.com>
2023-12-27 22:35:27 +0000

    Configure base rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { PieComponent } from './pie/pie.component';
+import { LastMonthComponent } from './last-month/last-month.component';
+import { LastSixMonthComponent } from './last-six-month/last-six-month.component';
+import { LastYearComponent } from './last-year/last-year.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pie', component: PieComponent},
+  { path: 'last-month', component: LastMonthComponent},
+  { path: 'last-six-months', component: LastSixMonthComponent},
+  { path: 'last-year', component: LastYearComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
